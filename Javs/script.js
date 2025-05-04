15a148391fa175bf3410ace5f3dc8e19169b66a5
Bona-CN <bonachidera65@gmail.com>
2025-05-04 21:44:41 +0100

    Update script.js

--- Javs/script.js
+++ Javs/script.js
@@ -2,39 +2,15 @@ let box = document.querySelector(".box");
 let circle = document.querySelector(".circle");
 
 
-
-// gsap.to([box, circle], {
-//     x:-500,
-//     rotation: 90,
-//     duration: 4
-// });
-
-gsap.to(box, {
+  gsap.to(circle, {
     x: -300,
     opacity: 0.5,
-    duration: 2,
+    duration: 1.5,
     delay: 0.5,
     ease: "bounce.out",
-    // onComplete: () => alert("Done!")
+    repeat: -1,
+    yoyo: true
   });
-
-//   gsap.to(box, {
-//     x: 300,
-//     opacity: 0.5,
-//     duration: 2,
-//     delay: 1,
-//     ease: "power2.in"
-//   });
-
-//   gsap.to(circle, {
-//     x: -300,
-//     opacity: 0.5,
-//     duration: 1.5,
-//     delay: 0.5,
-//     ease: "bounce.out",
-//     repeat: -1,
-//     yoyo: true
-//   });
 
 gsap.to(box, {
     y: "40vh",       // move UP 50% of the viewport height
